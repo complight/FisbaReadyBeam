complight/FisbaReadyBeam

commit c5efa3c2b91eee9f1c7f5e7202cd3661293c5553

Changed output format of construct_command() and input format to send_command() to un-encoded string. Changed output format of send_command() to un-encoded string for consistency. Moved sequence number insertion to send_command() to be able to send send command multiple times. Moved crc calculation to send_command() to accommodate for above change.

FisbaReadyBeam/__init__.py
@@ -90,18 +90,27 @@ class FisbaReadyBeam():
         Parameters
         ----------
         command           : str
-                            Command as an encoded string.
+                            Command as string.
         debug             : bool
                             Debug flag to observe the command and incoming data under a shell.
 
         Returns
         -------
         response_frame    : str
-                            Response frame as an encoded string.
+                            Response frame as string.
         """
         self.laser.reset_output_buffer()
         self.laser.reset_input_buffer()
-        self.laser.write(command)
+        # Fill placeholder with sequence number
+        self.sequence += 1
+        command = command.replace('----', '{:04X}'.format(self.sequence))
+        # Calculate checksum
+        crc_calculator = CrcCalculator(Crc16.CCITT)
+        checksum = crc_calculator.calculate_checksum(command.encode())
+        command += '{:04X}'.format(checksum)
+        command += '\r'
+        # Send command and receive answer
+        self.laser.write(command.encode())
         self.laser.flush()
         cr = "\r".encode()
         response_frame = b''
@@ -113,7 +122,7 @@ class FisbaReadyBeam():
         if debug:
             print(command.decode())
             print(response_frame.decode())
-        return response_frame
+        return response_frame.decode()
 
 
     def construct_command(self, parameter_id, value=None, instance=1):
@@ -132,11 +141,10 @@ class FisbaReadyBeam():
         Returns
         -------
         command            : str
-                             Command as an encoded string.
+                             Command as string.
         """
         command = '#{:02X}'.format(self.address)
-        self.sequence += 1
-        command += '{:04X}'.format(self.sequence)
+        command += '----' # Insert placeholder for sequence number
         if isinstance(value, type(None)):
             command += '?VR'
         elif not isinstance(value, type(None)):
@@ -148,11 +156,8 @@ class FisbaReadyBeam():
                 command += '{:08X}'.format(struct.unpack('<I', struct.pack('<f', value))[0])
             elif isinstance(value, int):
                 command += '{:08X}'.format(1)
-        crc_calculator = CrcCalculator(Crc16.CCITT)
-        checksum = crc_calculator.calculate_checksum(command.encode())
-        command += '{:04X}'.format(checksum)
-        command += '\r'
-        return command.encode()
+        
+        return command
 
     
     def set_brightness(self, power=[10., 0., 10.]):

commit 79fcac124b7ed72cf04eff8cc18d2ef442290317

Fix error during printing when debug=True introduced in c5efa3c2b91eee9f1c7f5e7202cd3661293c5553

FisbaReadyBeam/__init__.py
@@ -120,7 +120,7 @@ class FisbaReadyBeam():
             response_byte = self.read(size=1)
         response_frame = response_frame[1:]
         if self.debug:
-            print(command.decode())
+            print(command)
             print(response_frame.decode())
         return response_frame.decode()
 